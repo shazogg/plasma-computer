a2d7a338ac858cff7e72621ad61b02d01f39b8b1
shazogg <shawn.franceus@gmail.com>
2023-04-11 18:13:54 +0200

    Update README.md

--- os/demo_os.lua
+++ os/demo_os.lua
@@ -3,6 +3,3 @@
 function setup()
   output("0,0,0!display§!<color=#0000ff>PlamaOS v0.1</color>!§!<color=#00ff00>shazogg@plasma-computer</color>:<color=#0000ff>~</color>$", 1)
 end
-
-function loop()
-end
--- os/plasma_os.lua
+++ os/plasma_os.lua
@@ -1,5 +1,7 @@
 --OS!disk§!
 -- Global variables
+BACKGROUND_COLOR = {0, 0, 0}
+TEXT_COLOR = {255, 255, 255}
 SEPARATOR = "!§!"
 
 -- Split a string into a table of substrings
@@ -10,6 +12,29 @@ function split(str, sep)
       table.insert(segments, segment)
   end
   return segments
+end
+
+-- Convert a table of RGB values to a hex color
+function rgbToHex(rgb)
+  local hex = ""
+  for i = 1, #rgb do
+    local hexValue = string.format("%02X", rgb[i])
+    hex = hex .. hexValue
+  end
+  return hex
+end
+
+-- Colorize a string
+function colorizeText(text, color)
+  return "<color=#" .. color .. ">" .. text .. "</color>"
+end
+
+-- Separate a string at a specific position and add text at that position
+function separateString(str, position, char)
+  local str1 = string.sub(str, 1, position - 1)
+  local str2 = string.sub(str, position)
+  local separatedStr = str1 .. char .. str2
+  return separatedStr
 end
 
 -- Keyboard event
@@ -40,14 +65,34 @@ function readMemoryEvent()
   end
 end
 
+function textEditor()
+  if blink then
+    return separateString(current_editor_text, 6, "<color=#FFFFFF>|</color>")
+  else
+    return separateString(current_editor_text, 6, "<color=#" .. rgbToHex(BACKGROUND_COLOR) .. ">|</color>")
+  end
+end
+
 -- Setup
 function setup()
   start_check = true
+  count = 0
+  blink = false
+
+  current_editor_text = "hello world"
 end
 
 -- Loop
 function loop()
   -- Check if the start_check variable is not nil or false
   if start_check then
+    count = count + 1
+
+    if count >= 15 then
+      count = 0
+      blink = not blink
+    end
+
+    output(textEditor(), 1)
   end
 end
