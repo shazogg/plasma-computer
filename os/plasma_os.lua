ecafa4367c3c4ad73db06ded3634e3ec61cc2011
shazogg <shawn.franceus@gmail.com>
2023-04-11 21:19:53 +0200

    Update Plasma OS

--- os/plasma_os.lua
+++ os/plasma_os.lua
@@ -2,6 +2,7 @@
 -- Global variables
 BACKGROUND_COLOR = {0, 0, 0}
 TEXT_COLOR = {255, 255, 255}
+MAX_LINE_DISPLAY_LENGTH = 30
 SEPARATOR = "!§!"
 
 -- Split a string into a table of substrings
@@ -37,10 +38,42 @@ function separateString(str, position, char)
   return separatedStr
 end
 
+-- Remove a character from a string at a specific position
+function removeChar(str, position)
+  local str1 = string.sub(str, 1, position - 1)
+  local str2 = string.sub(str, position + 1)
+  local removedStr = str1 .. str2
+  return removedStr
+end
+
+-- Separate a string at a specific position and kee pthe end of the string
+function separateStringEnd(str, position)
+  return string.sub(str, position)
+end
+
 -- Keyboard event
 function keyboardEvent()
   if V1 ~= nil then
-    print(V1)
+    splited_data = split(V1, SEPARATOR)
+
+    -- Character pressed
+    if #splited_data == 2 then
+      if splited_data[1] == "CHAR" then
+        addEditorCharacter(splited_data[2])
+      end
+    -- Key pressed
+    elseif #splited_data == 1 then
+      if splited_data[1] == "BACKSPACE" then
+        print("BACKSPACE")
+        removeEditorCharacter()
+      elseif splited_data[1] == "DELETE" then
+        deleteEditorText()
+      elseif splited_data[1] == "ALEFT" then
+        moveCursorLeft()
+      elseif splited_data[1] == "ARIGHT" then
+        moveCursorRight()
+      end
+    end
   end
 end
 
@@ -65,21 +98,70 @@ function readMemoryEvent()
   end
 end
 
-function textEditor()
-  if blink then
-    return separateString(current_editor_text, 6, "<color=#FFFFFF>|</color>")
-  else
-    return separateString(current_editor_text, 6, "<color=#" .. rgbToHex(BACKGROUND_COLOR) .. ">|</color>")
+-- Add editor character
+function addEditorCharacter(char)
+  current_editor_text = separateString(current_editor_text, current_cursor_position, char)
+  moveCursorRight()
+end
+
+-- Remove editor character
+function removeEditorCharacter()
+  current_editor_text = removeChar(current_editor_text, current_cursor_position - 1)
+  moveCursorLeft()
+end
+
+-- Delete editor text
+function deleteEditorText()
+  current_editor_text = ""
+  current_cursor_position = 1
+end
+
+-- Move cursor left
+function moveCursorLeft()
+  if current_cursor_position > 1 then
+    current_cursor_position = current_cursor_position - 1
+    blink = true
   end
+end
+
+-- Move cursor right
+function moveCursorRight()
+  if current_cursor_position < string.len(current_editor_text) + 1 then
+    current_cursor_position = current_cursor_position + 1
+    blink = true
+  end
+end
+
+function textEditor()
+  displayed_text = current_editor_text
+
+  print(current_cursor_position)
+
+  if current_cursor_position > MAX_LINE_DISPLAY_LENGTH then
+    displayed_text = separateStringEnd(displayed_text, current_cursor_position - MAX_LINE_DISPLAY_LENGTH)
+    displayed_text = separateString(displayed_text, MAX_LINE_DISPLAY_LENGTH + 1, colorizeText("|", rgbToHex(blink and TEXT_COLOR or BACKGROUND_COLOR)))
+  else
+    displayed_text = separateString(displayed_text, current_cursor_position, colorizeText("|", rgbToHex(blink and TEXT_COLOR or BACKGROUND_COLOR)))
+  end
+
+  --print(string.len(displayed_text) .. "/" .. string.len(current_editor_text))
+
+  return displayed_text
 end
 
 -- Setup
 function setup()
+  -- Utility variable
   start_check = true
+
+  -- Blink cursor variables
   count = 0
   blink = false
 
-  current_editor_text = "hello world"
+  -- Text editor variables
+  current_editor_text = ""
+  current_cursor_position = 1
+  current_text_offset = 1
 end
 
 -- Loop
